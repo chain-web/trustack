26f84ab53f162802874fde017cc4976f2625fcae
scc <66scc66@gmail.com>
2023-01-30 09:11:03 +0000

    feat:basic fs

--- packages/skchain/src/mate/utils.ts
+++ packages/skchain/src/mate/utils.ts
@@ -5,7 +5,14 @@ import { sha256 as hasher } from 'multiformats/hashes/sha2';
 
 // 113 = codec of @ipld/dag-cbor
 // 18 = sha256 hasher code
-export type DefaultBlockType<T> = BlockView<T, 113, 18, 1>;
+export type DefaultBlockCodec = 113;
+export type DefaultBlockHasher = 18;
+export type DefaultBlockType<T> = BlockView<
+  T,
+  DefaultBlockCodec,
+  DefaultBlockHasher,
+  1
+>;
 
 // generate init account storageRoot
 export const createEmptyStorageRoot = async (): Promise<CID> => {
@@ -16,6 +23,19 @@ export const createEmptyStorageRoot = async (): Promise<CID> => {
 export const createBlock = async <T>(
   value: T,
 ): Promise<DefaultBlockType<T>> => {
-  const block = await Block.encode<T, 113, 18>({ value, codec, hasher });
+  const block = await Block.encode<T, DefaultBlockCodec, DefaultBlockHasher>({
+    value,
+    codec,
+    hasher,
+  });
   return block;
+};
+
+export const takeBlockValue = async <T>(data: Uint8Array): Promise<T> => {
+  const value = await Block.decode<T, DefaultBlockCodec, DefaultBlockHasher>({
+    bytes: data,
+    codec,
+    hasher,
+  });
+  return value.value;
 };
