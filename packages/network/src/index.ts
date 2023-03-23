b26a7d31c8c2e9ed2b2f9c7e1e7d281fd6f44ec8
scc <66scc66@gmail.com>
2023-03-23 18:31:11 +0800

    deps: update ipfs and libp2p deps (#63)

--- packages/network/src/index.ts
+++ packages/network/src/index.ts
@@ -3,6 +3,9 @@ import { createLibp2p } from 'libp2p';
 import type { Datastore } from 'interface-datastore';
 import { noise } from '@chainsafe/libp2p-noise';
 import type { PeerId } from '@libp2p/interface-peer-id';
+import { tcp } from '@libp2p/tcp';
+import { gossipsub } from '@chainsafe/libp2p-gossipsub';
+import { mplex } from '@libp2p/mplex';
 // import { multiaddr } from '@multiformats/multiaddr';
 // import first from 'it-first';
 // import { pipe } from 'it-pipe';
@@ -12,21 +15,31 @@ import type { PeerId } from '@libp2p/interface-peer-id';
 interface NetworkOptions {
   peerId: PeerId;
   datastore: Datastore;
+  tcpPort: number;
 }
 
 class network {
   constructor(opts: NetworkOptions) {
     this.peerId = opts.peerId;
     this.datastore = opts.datastore;
+    this.tcpPort = opts.tcpPort;
   }
   peerId: PeerId;
   datastore: Datastore;
+  tcpPort: number;
   node!: Libp2p;
 
   async start() {
     this.node = await createLibp2p({
       peerId: this.peerId,
-      datastore: this.datastore,
+      datastore: this.datastore as any, // TODO: fix this by wait libp2p update it types
+      addresses: {
+        listen: [`/ip4/0.0.0.0/tcp/${this.tcpPort}`],
+      },
+      streamMuxers: [mplex()],
+      connectionEncryption: [noise()],
+      transports: [tcp()],
+      pubsub: gossipsub(),
     });
   }
 }
